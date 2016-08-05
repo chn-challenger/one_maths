932640222587432ac5f26a3bdd6a7af844e7c30c
Joe Zhou <chn.challenger@gmail.com>
2016-08-05 15:01:49 +0100

    can create courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,4 +4,18 @@ class CoursesController < ApplicationController
     @courses = Course.all
   end
 
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.create(course_params)
+    redirect_to '/courses'
+  end
+
+  def course_params
+    params.require(:course).permit!
+  end
+
+
 end
--- spec/features/courses_feature_spec.rb
+++ spec/features/courses_feature_spec.rb
@@ -20,4 +20,16 @@ feature 'courses' do
       expect(page).not_to have_content('No courses added yet')
     end
   end
+
+  context 'creating courses' do
+    scenario 'prompts user to fill out a form, then displays the new course' do
+      visit '/courses'
+      click_link 'Add a course'
+      fill_in 'Name', with: 'A-Level Maths'
+      fill_in 'Description', with: '2 year course'
+      click_button 'Create Course'
+      expect(page).to have_content 'A-Level Maths'
+      expect(current_path).to eq '/courses'
+    end
+  end
 end

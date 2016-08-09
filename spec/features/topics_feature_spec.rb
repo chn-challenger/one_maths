9b56da2bb8ec70d57d5a48f19bae162ce5bd158c
Joe Zhou <chn.challenger@gmail.com>
2016-08-09 23:24:51 +0100

    topic tests amended

--- spec/features/topics_feature_spec.rb
+++ spec/features/topics_feature_spec.rb
@@ -9,9 +9,8 @@ feature 'topics' do
 
     scenario 'should display a prompt to add a topic' do
       sign_in_maker
-      expect(current_path).to eq '/'
-      expect(page).to have_content 'No topics have been added to Core 1'
-      expect(page).to have_link 'Add a topic to Core 1'
+      visit "/units/#{core_1.id}"
+      expect(page).to have_link 'Add a new chapter'
     end
   end
 
@@ -22,9 +21,8 @@ feature 'topics' do
     let!(:indices){create_topic(unit,maker)}
 
     scenario 'display the added topics' do
-      visit '/'
-      expect(page).to have_content 'Indices'
-      expect(page).to have_content 'blank'
+      visit "/units/#{unit.id}"
+      expect(page).to have_content 'Chapter 1 Indices'
     end
   end
 
@@ -34,42 +32,27 @@ feature 'topics' do
     let!(:unit){create_unit(course,maker)}
 
     scenario 'when not logged in cannot add topic' do
-      visit "/"
-      expect(page).not_to have_link "Add a topic to Core 1"
+      visit "/units/#{unit.id}"
+      expect(page).not_to have_link "Add a new chapter"
     end
 
     scenario 'a maker adding a unit to his course' do
       sign_in_maker
-      click_link 'Add a topic to Core 1'
+      visit "/units/#{unit.id}"
+      click_link "Add a new chapter"
       fill_in 'Name', with: 'Indices'
       fill_in 'Description', with: 'blank'
       click_button 'Create Topic'
       expect(page).to have_content 'Indices'
-      expect(page).to have_content 'blank'
-      expect(current_path).to eq '/'
+      expect(current_path).to eq "/units/#{unit.id}"
     end
 
     scenario 'a different maker cannot add a topic' do
       sign_up_tester
       visit "/units/#{unit.id}/topics/new"
-      expect(page).not_to have_link "Add a topic to Core 1"
+      expect(page).not_to have_link "Add a new chapter"
       expect(page).to have_content 'You can only add topics to your own unit'
-      expect(current_path).to eq '/'
-    end
-  end
-
-  context 'viewing topics' do
-    let!(:maker){create_maker}
-    let!(:course){create_course(maker)}
-    let!(:unit){create_unit(course,maker)}
-    let!(:indices){create_topic(unit,maker)}
-
-    scenario 'view the details of a topic' do
-      visit "/"
-      click_link 'View Indices'
-      expect(page).to have_content 'Indices'
-      expect(page).to have_content 'blank'
-      expect(current_path).to eq "/topics/#{indices.id}"
+      expect(current_path).to eq "/units/#{unit.id}"
     end
   end
 
@@ -81,21 +64,22 @@ feature 'topics' do
 
     scenario 'a maker can update his own topics' do
       sign_in_maker
-      click_link 'Edit Indices'
+      visit "/units/#{unit.id}"
+      click_link 'Edit chapter'
       fill_in 'Name', with: 'Algebra 1'
       fill_in 'Description', with: 'basic equations'
       click_button 'Update Topic'
       expect(page).to have_content 'Algebra 1'
       expect(page).to have_content 'basic equations'
-      expect(current_path).to eq '/'
+      expect(current_path).to eq "/units/#{unit.id}"
     end
 
     scenario "a maker cannot edit someone else's topics" do
       sign_up_tester
       visit "/topics/#{indices.id}/edit"
-      expect(page).not_to have_link 'Edit Indices'
+      expect(page).not_to have_link 'Edit chapter'
       expect(page).to have_content 'You can only edit your own topics'
-      expect(current_path).to eq "/"
+      expect(current_path).to eq "/units/#{unit.id}"
     end
   end
 
@@ -107,16 +91,17 @@ feature 'topics' do
 
     scenario 'a maker can delete their own topics' do
       sign_in_maker
-      click_link 'Delete Indices'
+      visit "/units/#{unit.id}"
+      click_link 'Delete chapter'
       expect(page).not_to have_content "Indices"
       expect(page).not_to have_content "blank"
-      expect(current_path).to eq "/"
+      expect(current_path).to eq "/units/#{unit.id}"
     end
 
     scenario "a maker cannot delete another maker's topics" do
       sign_up_tester
-      visit '/'
-      expect(page).not_to have_link 'Delete Indices'
+      visit "/units/#{unit.id}"
+      expect(page).not_to have_link 'Delete chapter'
       page.driver.submit :delete, "/topics/#{indices.id}",{}
       expect(page).to have_content 'Can only delete your own topics'
     end

1a8b3afdeb2dfbcbd3945f2ad8abb19a6d37f8f7
Joe Zhou <chn.challenger@gmail.com>
2016-08-09 23:13:49 +0100

    amended tests for units

--- spec/features/units_feature_spec.rb
+++ spec/features/units_feature_spec.rb
@@ -7,9 +7,8 @@ feature 'units' do
       maker = create_maker
       create_course(maker)
       sign_in_maker
-      expect(current_path).to eq '/'
-      expect(page).to have_content 'No units have been added for Science'
-      expect(page).to have_link 'Add a unit for Science'
+      click_link 'Science'
+      expect(page).to have_link 'Add a unit'
     end
   end
 
@@ -18,7 +17,7 @@ feature 'units' do
       maker = create_maker
       course = create_course(maker)
       create_unit(course,maker)
-      visit "/courses"
+      visit "/courses/#{course.id}"
       expect(page).to have_content('Core 1')
       expect(page).to have_content('Basic maths')
     end
@@ -29,25 +28,26 @@ feature 'units' do
     let!(:science){create_course(maker)}
 
     scenario 'when not logged in cannot add unit' do
-      visit "/"
-      expect(page).not_to have_link "Add a unit for Science"
+      visit "/courses/#{science.id}"
+      expect(page).not_to have_link "Add a unit"
     end
 
     scenario 'a maker adding a unit to his course' do
       sign_in_maker
-      click_link 'Add a unit for Science'
+      visit "/courses/#{science.id}"
+      click_link 'Add a unit'
       fill_in 'Name', with: 'Core 1'
       fill_in 'Description', with: 'Very simple maths'
       click_button 'Create Unit'
       expect(page).to have_content 'Core 1'
       expect(page).to have_content 'Very simple maths'
-      expect(current_path).to eq '/courses'
+      expect(current_path).to eq "/courses/#{science.id}"
     end
 
     scenario 'a different maker cannot add a unit' do
       sign_up_tester
       visit "/courses/#{science.id}/units/new"
-      expect(page).not_to have_link "Add a unit for Science"
+      expect(page).not_to have_link "Add a unit"
       expect(page).to have_content 'You can only add units to your own course'
       expect(current_path).to eq '/'
     end
@@ -59,10 +59,9 @@ feature 'units' do
     let!(:core_1){create_unit(course,maker)}
 
     scenario 'public can view units in a course' do
-      visit "/courses"
-      click_link 'View Core 1'
+      visit "/courses/#{course.id}"
+      click_link 'Basic maths'
       expect(page).to have_content 'Core 1'
-      expect(page).to have_content 'Basic maths'
       expect(current_path).to eq "/units/#{core_1.id}"
     end
   end
@@ -74,21 +73,22 @@ feature 'units' do
 
     scenario 'a maker can update his own units' do
       sign_in_maker
-      click_link 'Edit Core 1'
+      visit "/courses/#{course.id}"
+      click_link 'Edit'
       fill_in 'Name', with: 'Mechanics 1'
       fill_in 'Description', with: 'Basic physics'
       click_button 'Update Unit'
       expect(page).to have_content 'Mechanics 1'
-      expect(page).to have_content 'Basic physics'
-      expect(current_path).to eq '/'
+      expect(current_path).to eq "/courses/#{course.id}"
     end
 
     scenario "a maker cannot edit someone else's units" do
       sign_up_tester
+      visit "/courses/#{course.id}"
+      expect(page).not_to have_link 'Edit'
       visit "/units/#{core_1.id}/edit"
-      expect(page).not_to have_link 'Edit Core 1'
       expect(page).to have_content 'You can only edit your own units'
-      expect(current_path).to eq "/"
+      expect(current_path).to eq "/courses/#{course.id}"
     end
   end
 
@@ -99,16 +99,16 @@ feature 'units' do
 
     scenario 'a maker can delete their own unit' do
       sign_in_maker
-      click_link 'Delete Core 1'
+      visit "/courses/#{course.id}"
+      click_link 'Delete'
       expect(page).not_to have_content "Core 1"
-      expect(page).not_to have_content "Basic maths"
-      expect(current_path).to eq "/"
+      expect(current_path).to eq "/courses/#{course.id}"
     end
 
     scenario "a maker cannot delete another maker's units" do
       sign_up_tester
-      visit '/'
-      expect(page).not_to have_link 'Delete Core 1'
+      visit "/courses/#{course.id}"
+      expect(page).not_to have_link 'Delete'
       page.driver.submit :delete, "/units/#{core_1.id}",{}
       expect(page).to have_content 'Can only delete your own units'
     end

84e225cce3445041b9b3e0bc8322f911152e1b2d
Joe Zhou <chn.challenger@gmail.com>
2016-08-09 23:34:54 +0100

    amended lesson tests

--- spec/features/lessons_feature_spec.rb
+++ spec/features/lessons_feature_spec.rb
@@ -10,9 +10,8 @@ feature 'lessons' do
 
     scenario 'should display a prompt to add a lesson' do
       sign_in_maker
-      expect(current_path).to eq '/'
-      expect(page).to have_content 'No lessons have been added to Indices'
-      expect(page).to have_link 'Add a lesson to Indices'
+      visit "/units/#{unit.id}"
+      expect(page).to have_link 'Add a lesson to chapter'
     end
   end
 
@@ -23,44 +22,28 @@ feature 'lessons' do
     let!(:topic){create_topic(unit,maker)}
 
     scenario 'when not logged in cannot add a lesson' do
-      visit "/"
-      expect(page).not_to have_link "Add a lesson to Indices"
+      visit "/units/#{unit.id}"
+      expect(page).not_to have_link "Add a lesson to chapter"
     end
 
     scenario 'a maker adding a lesson to his topic' do
       sign_in_maker
-      click_link 'Add a lesson to Indices'
+      visit "/units/#{unit.id}"
+      click_link "Add a lesson to chapter"
       fill_in 'Name', with: 'Index multiplication'
       fill_in 'Description', with: 'times divide power again of indices'
       fill_in 'Video', with: "0QjF6A3Zwkk"
       click_button 'Create Lesson'
       expect(page).to have_content 'Index multiplication'
-      expect(page).to have_content 'times divide power again of indices'
-      expect(current_path).to eq '/'
+      expect(current_path).to eq "/units/#{unit.id}"
     end
 
     scenario 'a different maker cannot add a lesson' do
       sign_up_tester
       visit "/topics/#{topic.id}/lessons/new"
-      expect(page).not_to have_link "Add a lesson to Indices"
+      expect(page).not_to have_link "Add a lesson to chapter"
       expect(page).to have_content 'You can only add lessons to your own topics'
-      expect(current_path).to eq '/'
-    end
-  end
-
-  context 'viewing lessons' do
-    let!(:maker){create_maker}
-    let!(:course){create_course(maker)}
-    let!(:unit){create_unit(course,maker)}
-    let!(:topic){create_topic(unit,maker)}
-    let!(:lesson){create_lesson(topic,maker)}
-
-    scenario 'view the details of a lesson' do
-      visit "/"
-      click_link 'View Index multiplication'
-      expect(page).to have_content 'Index multiplication'
-      expect(page).to have_content 'times divide power again of indices'
-      expect(current_path).to eq "/lessons/#{lesson.id}"
+      expect(current_path).to eq "/units/#{unit.id}"
     end
   end
 
@@ -73,22 +56,22 @@ feature 'lessons' do
 
     scenario 'a maker can update his own lessons' do
       sign_in_maker
-      click_link 'Edit Index multiplication'
+      visit "/units/#{unit.id}"
+      click_link 'Edit lesson'
       fill_in 'Name', with: 'A lesson'
       fill_in 'Description', with: 'basic lesson'
       fill_in 'Video', with: 'QiI9exfA'
       click_button 'Update Lesson'
       expect(page).to have_content 'A lesson'
-      expect(page).to have_content 'basic lesson'
-      expect(current_path).to eq '/'
+      expect(current_path).to eq "/units/#{unit.id}"
     end
 
     scenario "a maker cannot edit someone else's lessons" do
       sign_up_tester
       visit "/lessons/#{lesson.id}/edit"
-      expect(page).not_to have_link 'Edit Index multiplication'
+      expect(page).not_to have_link 'Edit lesson'
       expect(page).to have_content 'You can only edit your own lessons'
-      expect(current_path).to eq "/"
+      expect(current_path).to eq "/units/#{unit.id}"
     end
   end
 
@@ -101,15 +84,15 @@ feature 'lessons' do
 
     scenario 'a maker can delete their own lessons' do
       sign_in_maker
-      click_link 'Delete Index multiplication'
+      visit "/units/#{unit.id}"
+      click_link 'Delete lesson'
       expect(page).not_to have_content 'Index multiplication'
-      expect(page).not_to have_content 'times divide power again of indices'
-      expect(current_path).to eq "/"
+      expect(current_path).to eq "/units/#{unit.id}"
     end
 
     scenario "a maker cannot delete another maker's lessons" do
       sign_up_tester
-      visit '/'
+      visit "/units/#{unit.id}"
       expect(page).not_to have_link 'Delete Index multiplication'
       page.driver.submit :delete, "/lessons/#{lesson.id}",{}
       expect(page).to have_content 'Can only delete your own lessons'

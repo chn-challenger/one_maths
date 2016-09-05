61cda33617c8e0359aa22ea8add3243b6b71240a
Joe Zhou <chn.challenger@gmail.com>
2016-09-05 17:17:42 +0100

    added some manual collapsables

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,6 +21,9 @@ function showSolutions() {
     $('.solution-link').show();
     $('.next-question').hide();
 
+
+
+
     $('.solution-link').on('click', function(event){
       $(this).parent('form').siblings('.next-question').show();
 
@@ -44,6 +47,7 @@ function showSolutions() {
         MathJax.Hub.Typeset();
       });
     });
+
 
     $('.next-question').on('click', function(event){
       event.preventDefault();
@@ -110,131 +114,25 @@ function showSolutions() {
           });
         });
 
-
       });
     });
+
+
+
+    $('.chapter-collapsable').on('click', function(event){
+      event.preventDefault();
+      if ($(this).next().is(':visible')){
+        $(this).next().hide();
+      } else {
+        $(this).next().show();
+      };
+      $(".topic-headings").css("margin-top","5px");
+    });
+
+
+
 
 
   });
   MathJax.Hub.Typeset();
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function showSolutions() {
-//   $(document).ready(function() {
-//
-//     $('.next-question').hide();
-//
-//     $('.solution-link').on('click', function(event){
-//       $('.solution-link').hide();
-//       $('.next-question').show();
-//       var postAddress = $(this).parent('form')[0].action;
-//       var choice = $(this).parent('form').find('input:checked[name="choice"]').val();
-//       var question_id = $(this).parent('form').find('input[name="question_id"]').val();
-//       var authenticity_token = $(this).parent('form').find('input[name="authenticity_token"]').val();
-//       event.preventDefault();
-//       var solutionDiv = $(this).siblings("#solution-latex");
-//       var correctDiv = $(this).siblings("#correct");
-//       $.post(postAddress, { 'choice': choice, 'question_id': question_id, 'authenticity_token': authenticity_token }, function(response){
-//         solutionDiv.text(response.question_solution);
-//         correctDiv.text(response.message);
-//         MathJax.Hub.Typeset();
-//       });
-//     });
-//
-//     $('.next-question').on('click', function(event){
-//       event.preventDefault();
-//
-//       $.get(this.href, function(response){
-//
-//         $('.question-div').children().eq(2).text(response.question.question_text);
-//         $('.question-div .edit_question').children().eq(2).val(response.question.id);
-//         var utfInput = $('.question-div .edit_question').children().eq(0);
-//         var tokenInput = $('.question-div .edit_question').children().eq(1);
-//         var questionIdInput = $('.question-div .edit_question').children().eq(2);
-//         var solutionLatexDiv = $('.question-div .edit_question').children().last();
-//         solutionLatexDiv.text('');
-//         $('.question-div .edit_question').children().last().remove();
-//         var correctDiv = $('.question-div .edit_question').children().last();
-//         correctDiv.text('');
-//         $('.question-div .edit_question').children().last().remove();
-//         var submitInput = $('.question-div .edit_question').children().last();
-//         $('.question-div .edit_question').children().last().remove();
-//
-//         $('.question-div .edit_question').empty();
-//
-//         $('.question-div .edit_question').append(utfInput);
-//         $('.question-div .edit_question').append(tokenInput);
-//         $('.question-div .edit_question').append(questionIdInput);
-//         var choices = response.choices;
-//         for (var i = 0, len = choices.length; i < len; i++) {
-//           $('.question-div .edit_question').append("<input type='radio' id='choice-"
-//             + choices[i].id +"' " + "name='choice' value=" + choices[i].correct
-//             +  ">" + "<span>" + choices[i].content + "</span>" + "<br>");
-//         };
-//         $('.question-div .edit_question').append(submitInput);
-//         $('.question-div .edit_question').append(correctDiv);
-//         $('.question-div .edit_question').append(solutionLatexDiv);
-//
-//         $('.next-question').hide();
-//         $('.solution-link').show();
-//
-//         $('.solution-link').on('click', function(event){
-//           $('.solution-link').hide();
-//           $('.next-question').show();
-//           var postAddress = $(this).parent('form')[0].action;
-//           var choice = $(this).parent('form').find('input:checked[name="choice"]').val();
-//           var question_id = $(this).parent('form').find('input[name="question_id"]').val();
-//           var authenticity_token = $(this).parent('form').find('input[name="authenticity_token"]').val();
-//           event.preventDefault();
-//           var solutionDiv = $(this).siblings("#solution-latex");
-//           var correctDiv = $(this).siblings("#correct");
-//           $.post(postAddress, { 'choice': choice, 'question_id': question_id, 'authenticity_token': authenticity_token }, function(response){
-//             solutionDiv.text(response.question_solution);
-//             correctDiv.text(response.message);
-//             MathJax.Hub.Typeset();
-//           });
-//         });
-//
-//       });
-//     });
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//   });
-//   MathJax.Hub.Typeset();
-// };

7c065c05173a81e88f63d4b95224b5126fc25215
Joe Zhou <chn.challenger@gmail.com>
2016-09-06 08:49:38 +0100

    refactored current_exp method

--- app/models/student_lesson_exp.rb
+++ app/models/student_lesson_exp.rb
@@ -3,22 +3,10 @@ class StudentLessonExp < ApplicationRecord
   belongs_to :lesson
 
   def self.current_exp(user,lesson)
-    if user.is_a?(User)
-      user_id = user.id
-    else
-      user_id = user
-    end
-    if lesson.is_a?(Lesson)
-      lesson_id = lesson.id
-    else
-      lesson_id = lesson
-    end
-    record = StudentLessonExp.where(user_id: user_id, lesson_id: lesson_id).first
-    if record.nil?
-      return 0
-    else
-      return record.lesson_exp
-    end
+    user_id = user.is_a?(User) ? user.id : user
+    lesson_id = lesson.is_a?(Lesson) ? lesson.id : lesson
+    record = where(user_id: user_id, lesson_id: lesson_id).first                              
+    record.nil? ? 0 : record.lesson_exp
   end
 
 

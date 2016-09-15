7459ff20f1537c18f348f95305c80fcbcce6018c
Joe Zhou <chn.challenger@gmail.com>
2016-09-15 11:12:07 +0100

    poltergeist seems working

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,12 @@ require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
 require 'support/database_cleaner'
 
+
+options = {js_errors: false}
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, options)
+end
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in

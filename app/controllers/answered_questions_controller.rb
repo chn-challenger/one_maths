5e1a928e708fa274ac0a93888e74667645aa058e
Joe Zhou <chn.challenger@gmail.com>
2016-09-17 22:18:01 +0100

    answered questions now can be viewed by admin

--- app/controllers/answered_questions_controller.rb
+++ app/controllers/answered_questions_controller.rb
@@ -2,18 +2,17 @@ class AnsweredQuestionsController < ApplicationController
 
   def answered_questions
     user = User.where(email:session[:student_email]).first
-    if !!user
+    if current_user && !!user && can?(:create, Question)
       records = AnsweredQuestion.where(user_id:user.id)
       @answered_questions = []
       records.each do |record|
-        @answered_questions << [Question.where(id: record.question_id).first, record.correct.to_s]
+        correct = record.correct ? "Answered correctly" : "Answered incorrectly"
+        @answered_questions << [Question.where(id: record.question_id).first, correct]
       end
     else
       @answered_questions = []
     end
   end
-
-  #<% @answered_questions.each do |answered_question|%>
 
   def get_student
     session[:student_email] = params[:email]

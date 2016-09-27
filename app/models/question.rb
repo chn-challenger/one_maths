cadcd3bcb4981114f44033c899c612148591a050
Joe Zhou <chn.challenger@gmail.com>
2016-09-27 18:15:31 +0100

    added images to questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/missing.png"
+  has_attached_file :image, :styles => { large:"450x450", :medium => "300x300>", :thumb => "100x100>" }, default_url: 'missing.png'
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   has_and_belongs_to_many :lessons

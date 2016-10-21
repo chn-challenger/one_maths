039c010d3d9f28de2e6ed50227f5022c5221e991
Ivan <ibbakalov@gmail.com>
2016-10-21 10:13:36 +0100

    Added extra test cases for TexParser

--- spec/non_js_tests/actions/tex_parser_spec.rb
+++ spec/non_js_tests/actions/tex_parser_spec.rb
@@ -5,6 +5,7 @@ require 'general_helpers'
 describe TexParser, type: :action do
   let(:parser) { TexParser }
   let(:tex_file) { Rails.root + "spec/fixtures/Questions_Differentiation.tex" }
+  let(:tex_lessons_file) { Rails.root + "spec/fixtures/lesson_questions.tex"}
 
   describe '#convert' do
     it "adds new questions, choices and answers records" do
@@ -12,6 +13,13 @@ describe TexParser, type: :action do
       expect { parser_with_file.convert }.to change{ Question.count }
       expect { parser_with_file.convert }.to change{ Choice.count }
       expect { parser_with_file.convert }.to change{ Answer.count }
+    end
+
+    it "does not add questions, choices and answers records" do
+      parser_with_file = parser.new(tex_lessons_file)
+      expect { parser_with_file }.not_to change{ Question.count }
+      expect { parser_with_file.convert }.not_to change{ Choice.count }
+      expect { parser_with_file.convert }.not_to change{ Answer.count }
     end
   end
 

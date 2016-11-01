e968780ca56e27078fcbfeaabbfb7abfdc8d8157
Joe Zhou <chn.challenger@gmail.com>
2016-11-01 14:46:05 +0000

    modified image size

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
-  has_attached_file :picture, :styles => { medium:"500x500>"}, default_url: 'missing.png'
+  has_attached_file :picture, :styles => { medium:"500x500>", large:"900x900>"}, default_url: 'missing.png'
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
   # belongs_to :questions
   # belongs_to :question

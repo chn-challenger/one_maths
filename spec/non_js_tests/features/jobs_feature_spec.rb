ee296c4ae8dae828510c3f6dc83eaab78002dba9
Joe Zhou <chn.challenger@gmail.com>
2016-11-02 09:26:10 +0000

    added more tests passing for creating jobs

--- spec/non_js_tests/features/jobs_feature_spec.rb
+++ spec/non_js_tests/features/jobs_feature_spec.rb
@@ -3,6 +3,7 @@ require 'general_helpers'
 
 feature 'questions' do
   let!(:admin)  { create_admin   }
+  let!(:student){ create_student }
   let!(:question_1){create_question(1)}
   let!(:choice_1){create_choice(question_1,1,false)}
   let!(:choice_2){create_choice(question_1,2,true)}
@@ -23,6 +24,37 @@ feature 'questions' do
       expect(page).to have_content 'question text 1'
       expect(current_path).to eq "/jobs"
     end
+
+    scenario 'creating job with an invalid example id' do
+      sign_in admin
+      visit "/jobs"
+      click_link 'Add A Job'
+      fill_in "Name", with: "Quadratic Equation Application Question"
+      fill_in "Description", with: "Very long description of the job"
+      fill_in "Example", with: "11"
+      click_button "Create Job"
+      expect(page).to have_content 'Quadratic Equation Application Question'
+      expect(page).to have_content 'Very long description of the job'
+      expect(page).not_to have_content 'question text'
+      expect(current_path).to eq "/jobs"
+    end
+
+    scenario 'student cannot create a job' do
+      sign_in student
+      visit "/jobs"
+      expect(page).not_to have_link 'Add A Job'
+      visit new_job_path
+      expect(page).not_to have_button 'Create Job'
+    end
+
+    scenario 'cannot create a job when not logged on' do
+      visit "/jobs"
+      expect(page).not_to have_link 'Add A Job'
+      visit new_job_path
+      expect(page).not_to have_button 'Create Job'      
+    end
   end
+
+
 
 end

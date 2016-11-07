db2038ae0f9391d7976f64250d4fab343b5d2565
Joe Zhou <chn.challenger@gmail.com>
2016-11-07 09:27:07 +0000

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161101132344) do
 
   # These are extensions that must be enabled in order to support this database
@@ -101,6 +100,14 @@ ActiveRecord::Schema.define(version: 20161101132344) do
     t.integer "tag_id"
     t.index ["image_id"], name: "index_images_tags_on_image_id", using: :btree
     t.index ["tag_id"], name: "index_images_tags_on_tag_id", using: :btree
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "example_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "lessons", force: :cascade do |t|

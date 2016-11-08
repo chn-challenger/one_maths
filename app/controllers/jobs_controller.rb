e70b914d1d69ee039a26a341defeb61705e757da
DXTimer <ibbakalov@gmail.com>
2016-11-08 15:26:31 +0000

    WIP-styling and display for job done

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,17 @@
 class JobsController < ApplicationController
+  include JobsHelper
 
   def index
     @jobs = Job.all.order('created_at')
+  end
+
+  def show
+    if can? :read, Job
+      @job = Job.find(params[:id])
+    else
+      flash[:notice] = 'You cannot view this page.'
+      redirect_to jobs_path
+    end
   end
 
   def new
@@ -12,30 +22,34 @@ class JobsController < ApplicationController
     end
   end
 
-  def show
-    if can? :read, Job
-      @job = Job.find(params[:id])
-      
+  def create
+    if can? :create, Job
+      example_question = Question.find(id_extractor(job_params[:example_id]))
+      job = Job.new(job_params)
+      job.examples << example_question
+      job.job_questions << create_job_questions(3)
+
+      if job.save!
+        job_questions_ids = job.job_questions.pluck(:id)
+        job.unit = create_job_test_env(job_questions_ids)
+        flash[:notice] = "You have successfully created a job listing."
+      else
+        flash[:notice] = 'Something went wrong when saving the job listing, please review console.'
+        redirect_back(fallback_location: jobs_path)
+      end
+
     else
-      flash[:notice] = 'You cannot view this page.'
+      flash[:notice] = 'Only admins can create a job'
     end
     redirect_to jobs_path
   end
 
-  def create
-    if can? :create, Job
-      Job.create(job_params)
-    else
-      flash[:notice] = 'Only admins can create a job'
-    end
-    redirect_to '/jobs'
-  end
-
-  def show
-
-  end
+  private
 
   def job_params
-    params.require(:job).permit(:name, :description, :example_id)
+    params.require(:job).permit(:name, :description,
+                                :example_id, :price,
+                                :duration, :creator_id
+    )
   end
 end
--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,26 @@
 module JobsHelper
+
+  def id_extractor(string)
+    string.split(/\s*,\s*/)
+  end
+
+  def create_job_questions(n)
+    result = []
+    n.times { result << Question.create! }
+    result
+  end
+
+  def create_job_test_env(question_ids)
+    desc = 'This is a standard component generated for testing purposes.'
+    unit = Unit.create!(name: 'Maths', description: desc )
+    topic = Topic.create!(name: 'Calculus', description: desc,
+                          level_one_exp: 1000, unit_id: unit.id
+                         )
+    lesson = Lesson.create!(name: 'Lesson 1', description: desc,
+                            pass_experience: 500, topic_id: topic.id
+                           )
+    lesson.questions << Question.find(question_ids)
+    lesson.save!
+    unit
+  end
 end

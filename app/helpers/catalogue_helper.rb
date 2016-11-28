af12afd6cd735cbe28bea7c0e518edc8f2142cdf
DXTimer <ibbakalov@gmail.com>
2016-11-28 20:30:16 +0000

    WIP method via .inject method

--- app/helpers/catalogue_helper.rb
+++ app/helpers/catalogue_helper.rb
@@ -5,28 +5,9 @@ module CatalogueHelper
 
   def get_filtered_images(tag_names)
     tags = Tag.where(name: tag_names)
-    if even?(tag_names)
-      image_ids = tags.each {|tag, next_tag| tag.image_ids & next_tag.image_ids}
-      Image.find(image_ids)
-    else
-      arr_chunks = tags.size - (tags.size % 2)
-      tags_array = tags.each_slice(arr_chunks).to_a
-      p tags_array
-      odd_array = tags_array.pop
-
-      odd_image_ids = []
-      odd_array.each do |tag|
-        odd_image_ids << tag.image_ids
-      end
-      p odd_image_ids.flatten
-
-      image_ids = tags_array.each {|tag, next_tag| tag.image_ids & next_tag.image_ids}
-      p image_ids
-      Image.find(image_ids & odd_image_ids)
-    # else
-    #   Image.joins(:tags).where(tags: { name: tag_names }).distinct
-    # end
-    end
+    return [] unless !tags.empty?
+    image_ids = tags.inject(tags[-1].image_ids) { |result, tag| result & tag.image_ids}
+    Image.find(image_ids)
   end
 
   def even?(arr)

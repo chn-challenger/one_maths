73ab3432b238dc397105d7e065c1cbf32415bbfd
Joe Zhou <chn.challenger@gmail.com>
2016-12-08 19:38:58 +0000

    bug found and fixed

--- app/actions/update_exp.rb
+++ app/actions/update_exp.rb
@@ -2,57 +2,37 @@ module UpdateExp
 
   def update_correctness(amended_ans_q)
     future_answers = AnsweredQuestion.where(user_id:amended_ans_q.user_id,lesson_id:amended_ans_q.lesson_id, created_at: amended_ans_q.created_at..Time.now)
-    # future_answers = AnsweredQuestion.all
-    lesson_exp = StudentLessonExp.find_by(lesson_id: amended_ans_q.lesson_id, user_id: amended_ans_q.user_id)
-
-
-    arr_size = future_answers.size
-
-    # p arr_size
-
-    for i in 0...arr_size-1 do
-      # p i
-      if arr_size == i
-        current_correctness = future_answers[i].correctness
-        current_streak_mtp = future_answers[i].streak_mtp
-
-        if current_correctness < 0.99
-          lesson_exp.streak_mtp = ((current_streak_mtp - 1) * current_correctness) + 1
-        else
-          lesson_exp.streak_mtp = min(2,lesson_exp.streak_mtp + 0.25)
-        end
-        lesson_exp.save!
+    # lesson_exp = StudentLessonExp.find_by(lesson_id: amended_ans_q.lesson_id, user_id: amended_ans_q.user_id)
+    for i in 1...future_answers.length do
+      prev_correctness = future_answers[i-1].correctness
+      prev_streak_mtp = future_answers[i-1].streak_mtp
+      next_ans = future_answers[i]
+      if prev_correctness < 0.99
+        next_ans.streak_mtp = ((prev_streak_mtp - 1) * prev_correctness ) + 1
       else
-        current_correctness = future_answers[i].correctness
-        current_streak_mtp = future_answers[i].streak_mtp
-
-        puts "current streak is #{current_streak_mtp}"
-
-        next_ans = future_answers[i+1]
-
-        puts "current i+1 is #{i+1}"
-        puts "double check current #{future_answers[i+1].streak_mtp}"
-        # puts "current object id is #{future_answers[i].object_id}"
-        # puts "next object id is #{future_answers[i+1].object_id}"
-        puts "current i streak_mtp is #{next_ans.streak_mtp}"
-        # p next_ans
-
-        if current_correctness < 0.99
-          puts "first one"
-          next_ans.streak_mtp = ((current_streak_mtp - 1) * current_correctness) + 1
-          next_ans.save!
-        else
-          puts "second one"
-          next_ans.streak_mtp = [2,next_ans.streak_mtp + 0.25].min
-          next_ans.save!
-          puts next_ans.streak_mtp
-        end
-        # next_ans.save!
-        puts "updated i streak_mtp is #{next_ans.streak_mtp}"
-        puts "double check streak_mtp is #{future_answers[i+1].streak_mtp}"
-        puts '==================='
+        next_ans.streak_mtp = [prev_streak_mtp + 0.25,2].min
       end
+      next_ans.save!
     end
+
+
+
+
   end
 
 end
+
+
+
+
+    # for i in 0...future_answers.length - 1 do
+    #   prev_correctness = future_answers[i].correctness
+    #   prev_streak_mtp = future_answers[i].streak_mtp
+    #   next_ans = future_answers[i+1]
+    #   if prev_correctness < 0.99
+    #     next_ans.streak_mtp = ((prev_streak_mtp - 1) * prev_correctness) + 1
+    #   else
+    #     next_ans.streak_mtp = [NEXT_ANS.streak_mtp + 0.25,2].min
+    #   end
+    #   next_ans.save!
+    # end
--- spec/non_js_tests/actions/update_exp_spec.rb
+++ spec/non_js_tests/actions/update_exp_spec.rb
@@ -13,19 +13,18 @@ describe 'UpdateExp' do
   let!(:amended_ans_q) { create_ans_q(student, question_1, 0.5, 2, lesson) }
 
   describe '#update_correctness' do
-    # it 'recalculates one answered question' do
-    #   ans_q = create_ans_q(student, question_2, 1, 1, lesson)
-    #   updater.update_correctness(amended_ans_q)
-    #   expect(AnsweredQuestion.last.object_id).not_to eq ans_q.object_id
-    #   expect(AnsweredQuestion.last.streak_mtp).to eq 1.5
-    # end
+    it 'recalculates one answered question' do
+      ans_q = create_ans_q(student, question_2, 1, 1, lesson)
+      updater.update_correctness(amended_ans_q)
+      expect(AnsweredQuestion.last.object_id).not_to eq ans_q.object_id
+      expect(AnsweredQuestion.last.streak_mtp).to eq 1.5
+    end
 
     it 'recalculates two answered questions' do
       ans_q_12 = create_ans_q(student, question_2, 1, 1, lesson)
       ans_q_13 = create_ans_q(student, question_3, 1, 1.25, lesson)
       updater.update_correctness(amended_ans_q)
-      expect(AnsweredQuestion.last.streak_mtp).to eq 1.5
+      expect(AnsweredQuestion.last.streak_mtp).to eq 1.75
     end
-
   end
 end

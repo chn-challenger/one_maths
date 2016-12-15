f72b3086676895693419052be2a4c8f99c385219
DXTimer <ibbakalov@gmail.com>
2016-12-15 12:15:16 +0000

    WIP topics can be unlocked with specs

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,2 +1,20 @@
 module TopicsHelper
+
+  def topic_unlocked?(lessons, current_user)
+    lesson_pass_exps = lessons.order(:id).pluck(:pass_experience)
+    unlocked = false
+    lesson_exp = nil
+
+    lessons.order(:id).pluck(:id).each_with_index do |lesson_id, i|
+      lesson_exp = StudentLessonExp.find_by(user_id: current_user.id, lesson_id: lesson_id)
+
+      return false if lesson_exp.blank?
+
+      return false if !(lesson_exp.exp >= lesson_pass_exps[i])
+
+      unlocked = true
+    end
+
+    unlocked
+  end
 end

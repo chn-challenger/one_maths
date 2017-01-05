2e62a05a5e9372d3c0b4e88f25b15a79b7f8b433
DXTimer <ibbakalov@gmail.com>
2017-01-05 17:27:08 +0000

    WIP styled login/sign-up form

--- app/helpers/choices_helper.rb
+++ app/helpers/choices_helper.rb
@@ -1,2 +1,10 @@
 module ChoicesHelper
+
+  def choice_label(choice)
+     if choice.images.length > 0
+      image_tag choice.images.first.picture.url(:medium), class: "choice-image"
+     else
+      choice.content
+     end
+  end
 end

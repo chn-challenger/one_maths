160ab712fd8eafe8fe9676ffcbf5c91f4f5973c6
DXTimer <ibbakalov@gmail.com>
2017-01-17 19:06:28 +0000

    Bump refactoring topic ajax specs

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -11,12 +11,14 @@ module TopicsHelper
     unlocked = false
     lesson_exp = nil
 
-    lessons.order(:id).pluck(:id).each_with_index do |lesson_id, i|
-      lesson_exp = StudentLessonExp.find_by(user_id: current_user.id, lesson_id: lesson_id)
+    lessons.order(:id).each_with_index do |lesson, i|
+      lesson_exp = StudentLessonExp.find_by(user_id: current_user.id, lesson_id: lesson.id)
 
-      return false if lesson_exp.blank?
+      break if lesson_exp.blank?
 
-      return false if !(lesson_exp.exp >= lesson_pass_exps[i])
+      break if lesson.pass_experience == 0
+
+      break if !(lesson_exp.exp >= lesson_pass_exps[i])
 
       unlocked = true
     end

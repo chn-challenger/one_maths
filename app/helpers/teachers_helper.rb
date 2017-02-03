db750c29f69ae8493235a617a97a4b96672880d7
DXTimer <ibbakalov@gmail.com>
2017-02-03 17:17:16 +0000

    WIP added burst to topics and units pending specs and styling

--- app/helpers/teachers_helper.rb
+++ app/helpers/teachers_helper.rb
@@ -4,4 +4,20 @@ module TeachersHelper
     user.homework.include?(lesson)
   end
 
+  def has_homework_for?(record)
+    return false if !current_user.present? || current_user.homework.blank?
+
+    if record.is_a?(Unit)
+      return current_user.homework.map { |lesson| lesson.unit }.include?(record)
+    end
+
+    if record.is_a?(Topic)
+      return current_user.homework.map { |lesson| lesson.topic }.include?(record)
+    end
+
+    if record.is_a?(Lesson)
+      return current_user.homework.include?(record)
+    end
+  end
+
 end

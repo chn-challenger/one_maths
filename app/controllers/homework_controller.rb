73dcc2e995a1205c3dcd62a886c68efd20ab7526
DXTimer <ibbakalov@gmail.com>
2017-02-08 16:35:26 +0000

    WIP homework submission form done... specs pending

--- app/controllers/homework_controller.rb
+++ app/controllers/homework_controller.rb
@@ -1,4 +1,8 @@
 class HomeworkController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :set_student, only: :create
+
   def index
   end
 
@@ -18,5 +22,11 @@ class HomeworkController < ApplicationController
   end
 
   def new
+  end
+
+  private
+
+  def homework_params
+    params.require(:homework).permit(:student_id, :lesson_id, :topic_id, :target_exp)
   end
 end
--- app/models/homework.rb
+++ app/models/homework.rb
@@ -2,7 +2,7 @@ class Homework < ApplicationRecord
   belongs_to :student, class_name: 'User'
   belongs_to :lesson, optional: true
   belongs_to :topic, optional: true
-  belongs_to :teacher, class_name: 'User', through: :student
+  # belongs_to :teacher, class_name: 'User', through: :student
 
   has_many :answered_questions
   has_many :questions, through: :answered_questions
